f8180c4b563a8136b3b4ed767442bbe31879d57d
tgordon2020 <63424810+tgordon2020@users.noreply.github.com>
2020-05-23 13:53:51 -0400

    Update virl-parse.py

--- virl-parse.py
+++ virl-parse.py
@@ -2,11 +2,20 @@ from lxml import etree as ET
 import glob
 import re
 import os
+from collections import defaultdict
+
 
 file_pattern = input("pattern to search: ")
 file_list = glob.glob(f'*{file_pattern}*.virl')
 
 for file_name in file_list:
+
+    node_interface = defaultdict(list)
+    node_details = defaultdict(list)
+    connections = []
+    nodes = []
+    node = ""
+
     with open(file_name) as file:
         virl_xml = file.read()
     directory = file_name.replace(".", "_")
@@ -15,49 +24,45 @@ for file_name in file_list:
     except:
         pass
 
+    # remove the <!--comment-->, which causes the parse to crash
+    virl_xml = re.sub(r"<!--.+-->", "", virl_xml)
     root = ET.fromstring(bytes(virl_xml, encoding='utf-8'))
 
-    nodenumber = 0
-    node = []
-    nodetype = {}
-    interface = {}
-    connections = []
+    # remove namespace
+    for elem in root.getiterator():
+        elem.tag = ET.QName(elem).localname
+        ET.cleanup_namespaces(root)
 
-    for x in root:
-        if "node" in x.tag:
-            node.append(x.get('name'))
-            nodenumber += 1
-            interface[x.get('name')] = [y.get('name') for y in x if 'extensions' not in y.tag]
-            nodetype[x.get('name')]=x.get('subtype')
-            try:
-                device_config = (x[0][0].text)
-                with open(f"{directory}\/{x.get('name')}.txt", "w") as config:
-                    config.write(device_config)
-            except Exception as e:
-                print(e)
+    # extract node, interface, and config info from xml
+    for x in root.xpath("//node|//interface|//entry[@key='config']"):
+        if x.tag == "node":
+            node = x.attrib['name']
+            node_details[node] = x.attrib['subtype']
+            nodes.append(node)
+        elif x.tag == "interface":
+            node_interface[node].append(x.attrib['name'])
+        elif x.tag == "entry":
+            with open(f"{directory}/{node}-config.txt", "w") as config:
+                config.write(x.text)
 
-        elif "connection" in x.tag:
-            dst= x.get('dst')
-            src =x.get('src')
-            dstint = re.findall("[0-9]{1,2}", dst)
-            srcint = re.findall("[0-9]{1,2}", src)
-            condst = (int(dstint[0])-1),(int(dstint[1])-1)
-            consrc = (int(srcint[0])-1),(int(srcint[1])-1)
-            dstnode=node[condst[0]]
-            dstinter=interface[dstnode][condst[1]]
-            srcnode=node[consrc[0]]
-            srcinter=interface[srcnode][consrc[1]]
-            connections.append(f"{dstnode} {dstinter} -- {srcnode} {srcinter}")
+    # device interface connections
+    for y in root.xpath("//connection"):
+        dst_conn = re.findall("[0-9]{1,2}", str(y.attrib['dst']))
+        src_conn = re.findall("[0-9]{1,2}", str(y.attrib['src']))
+        src_node = nodes[int(src_conn[0])-1]
+        dst_node = nodes[int(dst_conn[0])-1]
+        src_int = node_interface[src_node][int(src_conn[1])-1]
+        dst_int = node_interface[dst_node][int(dst_conn[1])-1]
+        connections.append(f"{src_node},{src_int},{dst_node},{dst_int}")
 
-        else:
-            pass
 
-    with open(f"{directory}\/devices.txt",'w') as device_info:
-        for k,v in interface.items():
-            device_info.write(f'{k} : {nodetype[k]}\n')
-            for a in v:
-                device_info.write(f'    {a}\n')
+    with open (f"{directory}/devices.csv", "w") as device_file:
+        for d, e in node_interface.items():
+            device_file.write(f"{d},{node_details[d]},")
+            for f in e:
+                device_file.write(f"{f},")
+            device_file.write("\n")
 
-    with open(f"{directory}\/connections.txt",'w') as device_link:
-        for con in connections:
-            device_link.write(f'{con}\n')
+    with open(f"{directory}/connections.csv", "w") as connections_file:
+        for a in connections:
+            connections_file.write(f"{a}\n")
